6163a7316675cecfdf916d26956e17d5c6a68e7a
Celia Alfaya Sánchez <celyurin@MBP-de-Celia.home>
2022-12-03 00:05:25 +0100

    primer commit let's hope for the best

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'DTmQWuwrNq6d2RZem7PlW9Qx2',  
+    consumer_secret: 'YhaiKHur6CB3f1jSPqBH8SDDK5rUpxwR0y5myVD7MYQQ5a58FC',
+    access_token: '1333392892019269634-eN6F4ApkY9An3qCpi4hItoPb1lZvl8',  
+    access_token_secret: 'XN4tBh7Vvzla95KzlIKJfV2FbwoLiuTvrL4yZWzm6xtiP'
+  }
---
+++ twit.js
@@ -0,0 +1,10 @@
+const twit = require("twit");
+
+const T = new twit({
+  consumer_key: "DTmQWuwrNq6d2RZem7PlW9Qx2",
+  consumer_secret: "YhaiKHur6CB3f1jSPqBH8SDDK5rUpxwR0y5myVD7MYQQ5a58FC",
+  access_token: "1333392892019269634-eN6F4ApkY9An3qCpi4hItoPb1lZvl8",
+  access_token_secret: "XN4tBh7Vvzla95KzlIKJfV2FbwoLiuTvrL4yZWzm6xtiP",
+});
+
+module.exports = T;
